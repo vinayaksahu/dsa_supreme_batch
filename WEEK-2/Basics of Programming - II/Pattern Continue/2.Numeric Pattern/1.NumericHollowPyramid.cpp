6e43d86c0cf3673b381c5a78663a5264b542c9d3
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-24 13:52:45 +0530

    organizing folder

---
+++ WEEK-2/Basics of Programming - II/Pattern Continue/2.Numeric Pattern/1.NumericHollowPyramid.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n = 3;
+    int row = n + 2;
+    int col = row + 2;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            
+        } cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+
+*1*
+*121*
+*12321*
+*121*
+*1*  
+
+*/
