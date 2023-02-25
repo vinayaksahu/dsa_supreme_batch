8067d055359ecfe0e13bc8db8d1aecc6cf2bf5a8
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-25 16:56:44 +0530

    even odd

---
+++ WEEK-1/Basics of Programming - I/Conditionals & Loops/Class Work/4.checkOddEven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int num = 4455;
+
+    if (num % 2 == 0)
+        cout << "Given number is even";
+    else   
+        cout << "Given number id odd";
+
+    return 0;
+}
