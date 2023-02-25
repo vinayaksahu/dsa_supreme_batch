8fbb9008d93038e0322e1172df1273589685ef31
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-25 20:29:06 +0530

    factorial of a number

--- WEEK-1/Basics of Programming - I/Conditionals & Loops/Home Work/5.factorial.cpp
+++ WEEK-1/Basics of Programming - I/Conditionals & Loops/Home Work/5.factorial.cpp
@@ -6,22 +6,28 @@ int main()
     int n = 5;
     //cin >> n;
 /* 
-    int fact = 1; // let factorial of given number is n.
+    int fact = 1; // let factorial of given number is 1.
 
     for (int i = 1; i <= n; i++)
     {
         fact = fact * i; // 1*2*3*4*5
     }
  */
-
-    int fact = n;
+/* 
+    int fact = n; // let factorial of given number is n.
 
     for (int i = n-1; i >= 1; i--)
     {
         fact = fact * i;  // 5*4*3*2*1
     }
-     
+      */
 
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+            
 
     //print fact
     cout << "factorial of given number is : " << fact;    
