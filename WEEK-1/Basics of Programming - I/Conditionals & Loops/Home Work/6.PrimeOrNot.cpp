fc7d0efe6317574b83ec617ba3e4b3bea1e624f5
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-25 21:20:31 +0530

    prime or not

--- WEEK-1/Basics of Programming - I/Conditionals & Loops/Home Work/6.PrimeOrNot.cpp
+++ WEEK-1/Basics of Programming - I/Conditionals & Loops/Home Work/6.PrimeOrNot.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 int main()
-{
-    int n = 5, flag = 0;
-    //cin >> n; // n is factorail or not
+{ // approch - if n is not prime then it can be devide by i = 2,3,4
+    int n = 5;
+    bool flag = true;
 
+    if (n == 0 || n == 1)
+        flag = false;
+    
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) { // if n is not prime then it can be devide from i = 2,3,4
+            cout << "Not Prime";
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << "Prime";
+    else
+        cout << "Not Prime";
+
+    
+    
+
+    //cin >> n; // n is factorail or not
+/* 
     for (int i = 2; i < n; i++) // is 5 divide by 2, 3, 4 or not
     {
         if (n % i == 0) // if dividing by 2, 3, 4 ans is 0
@@ -19,7 +40,7 @@ int main()
     if (flag == 0)  // if flag remains 0 (false) means 5 is not divided by 2, 3, 4
     {
         cout << "Prime";    //it is prime
-    }
+    } */
 
     return 0;
 }
