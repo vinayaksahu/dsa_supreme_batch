a493614fe620daf19502fb3b2a209d8236359f8e
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-25 15:24:48 +0530

    difference of 2 numbers

---
+++ WEEK-1/Basics of Programming - I/Conditionals & Loops/Class Work/2.DifferenceOf2Num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int num1 = 4;
+    int num2 = 6;
+
+    int diff = num1 - num2;
+
+    cout << "Differece of 2 num: " << diff; 
+
+    return 0;
+}
