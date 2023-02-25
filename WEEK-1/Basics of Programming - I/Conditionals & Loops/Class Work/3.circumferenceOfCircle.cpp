d437dc27ea0ba976766a8432e00783c7468f8a47
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-25 15:28:44 +0530

    circumference of a circle

---
+++ WEEK-1/Basics of Programming - I/Conditionals & Loops/Class Work/3.circumferenceOfCircle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int radios = 4;
+
+    int circumference = 2 * 3.14 * radios;
+
+    cout << "circumference of a circle: " << circumference; 
+
+    return 0;
+}
