643337d68848a5ddb39f04e02940767836b3082c
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-27 13:52:59 +0530

    left half pyramid

--- 1.Basics of Programming - I/3.Let's solve some Patterns/Class - Pattern Printing/3.LeftHalfPyramid.cpp
+++ 1.Basics of Programming - I/3.Let's solve some Patterns/Class - Pattern Printing/3.LeftHalfPyramid.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 int main() {
-    
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < i; j++) {
+
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i+1; j++) {
             cout << "*";
         } cout << endl;
     }
+    
+
+    
+/*     for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        } cout << endl;
+    } */
         
 
     return 0;
