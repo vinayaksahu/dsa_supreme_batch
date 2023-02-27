784e110d55df41c3540882a9611e06ff362bd810
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-27 13:55:57 +0530

    inverted left half pyramid

--- 1.Basics of Programming - I/3.Let's solve some Patterns/Class - Pattern Printing/4.InvertedLeftHalfPyramid.cpp
+++ 1.Basics of Programming - I/3.Let's solve some Patterns/Class - Pattern Printing/4.InvertedLeftHalfPyramid.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 int main() {
+
     int n = 5;
+
+    for (int  i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            cout << "*";
+        } cout << endl; 
+    }
+    
+
+
+
+/*     int n = 5;
     for (int i = 0; i < n; i++) { //row count
         for (int j = 0; j < n-i; j++) { // in every row print * according to number of row
             cout << "*";
         } cout << endl; //new line
-    }
+    } */
     return 0;
 }
