7ce0d464c91e060d0264cfb5ccd7a8a1043d01e7
Vinayaksahu <vinayaksahu3@gmail.com>
2023-02-28 21:44:32 +0530

    left numeric hollow pyramid odd number with spaces

--- 2.Week2 - Assignments/1.NumericHollow.cpp
+++ 2.Week2 - Assignments/1.NumericHollow.cpp
@@ -6,6 +6,34 @@ int main() {
     int n = 5;
 
     for (int i = 0; i < n; i++) {
+
+        if (i == n-1) {
+            for (int j = 0; j < n; j++) {
+                cout << 2*j+1 << " ";
+            }
+        }
+        
+        else {
+            for (int j = 0; j < 2*i+1; j++) {
+                if (j == 0 || j == 2*i)
+                    cout << j+1;
+                else    
+                    cout << " ";
+            }
+        } cout << endl;
+        
+    }
+/*
+1
+1 3
+1   5
+1     7
+1 3 5 7 9 
+*/
+    
+
+
+/*     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i+1; j++) {
             if (i == n-1 || j == 0 || j == i) 
                 cout << j+1<< " ";
@@ -15,7 +43,7 @@ int main() {
         }
         cout << endl;
     }
-    
+     */
 
     return 0;
 }
