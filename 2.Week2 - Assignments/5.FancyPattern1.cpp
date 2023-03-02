6192c85d6c9e604116d872ebeb9253f7c9e4727a
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-02 14:06:00 +0530

    fancy pattern

--- 2.Week2 - Assignments/5.FancyPattern1.cpp
+++ 2.Week2 - Assignments/5.FancyPattern1.cpp
@@ -3,9 +3,25 @@ using namespace std;
 
 int main() {
 
-    int n = 3;
+    int n = 5;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { //number of rows
+        int num_idx = 8-i; //num printing starting index
+        int num = i+1; //num that has to be print
+        int count_num = num; //
+        for (int j = 0; j < 17; j++) { //total number of element in every element 
+            if (j == num_idx && count_num > 0) {
+                cout << num;
+                num_idx += 2;
+                count_num--;
+            }
+            else
+                cout << "*";
+        } cout << endl; 
+    }
+    
+
+/*     for (int i = 0; i < n; i++) {
         int start_num_index = 8-i;
         int num = i+1;
         int count_num = num;
@@ -19,7 +35,7 @@ int main() {
             }
         }
         cout << endl;
-    }
+    } */
 
     return 0;
 }
