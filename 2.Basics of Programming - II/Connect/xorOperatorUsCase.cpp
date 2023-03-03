90ed8a01a69c7c7e82fa4f7a5559df2174a760b6
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-03 16:42:41 +0530

    xor operator use case

---
+++ 2.Basics of Programming - II/Connect/xorOperatorUsCase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 1;
+    int b = 1;
+    int c = 2;
+    int d = 3;
+    int e = 3;
+
+    int ans = a^b^c^d^e;
+
+    cout << ans;
+
+    return 0;
+}
