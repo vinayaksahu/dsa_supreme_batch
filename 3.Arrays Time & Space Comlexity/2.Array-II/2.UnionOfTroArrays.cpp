cd5e785376be64f3ca38f8e8f30365d59e877a6e
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-07 20:16:06 +0530

    common elements of 2 arrays

--- 3.Arrays Time & Space Comlexity/2.Array-II/2.UnionOfTroArrays.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II/2.UnionOfTroArrays.cpp
@@ -1,43 +1,35 @@
 #include<iostream>
 #include<vector>
-#include<limits.h>
 using namespace std;
 
 void printArray(vector<int> ans) {
 
-    for(auto value : ans) {
-        cout << value << " ";
+    for(auto val : ans) {
+        cout << val << " ";
     } cout << endl;
 }
 
 void findUnion(vector<int> arr, vector<int> brr) {
-    
+
     vector<int> ans;
 
-    //push all elements of arr to ans
+    //insert elements of arr to ans
     for (int i = 0; i < arr.size(); i++) {
         ans.push_back(arr[i]);
     }
-
-    //push all elements of brr to ans
-    for (int i = 0; i < ans.size(); i++) {
-        int element = ans[i];
-        for (int j = 0; j < brr.size(); j++) {
-            if (element != brr[j]) {
-                ans.push_back(brr[j]);
-            }
-        }
+    //again insert brr elements to ans
+    for (int i = 0; i < brr.size(); i++) {
+        ans.push_back(brr[i]);
     }
     
-
-    //print ans 
+    //print array
     printArray(ans);
 }
 
 int main() {
-    
-    vector<int> arr{2, 4, 6, 8, 7};
-    vector<int> brr{1, 3, 5, 7};
+
+    vector<int> arr{1,3,5,7};
+    vector<int> brr{2,4,6,8};
 
     findUnion(arr, brr);
 
--- 3.Arrays Time & Space Comlexity/2.Array-II/3.Intersection.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II/3.Intersection.cpp
@@ -5,37 +5,34 @@ using namespace std;
 
 void printArray(vector<int> ans) {
 
-    for(auto value : ans) {
-        cout << value << " ";
+    for(auto val : ans) {
+        cout << val << " ";
     } cout << endl;
 }
 
-void findIntersection(vector<int> arr, vector<int> brr) {
+void findIntersection (vector<int> arr, vector<int> brr) {
 
     vector<int> ans;
 
-    //for every elements of arr
+    //every element of arr will traverse brr
     for (int i = 0; i < arr.size(); i++) {
         int element = arr[i];
-        //we'll traverse entire array of brr
         for (int j = 0; j < brr.size(); j++) {
             if (element == brr[j]) {
-                ans.push_back(arr[i]);
-                break;           
-                //or we can mark
-                //brr[j] = INT_MIN;
+                ans.push_back(brr[j]);
+                brr[j] = INT_MIN;
             }
         }
     }
 
-    //printArray
+    //print array
     printArray(ans);
 }
 
 int main() {
 
-    vector<int> arr{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> brr{2, 4, 6, 8, 9, 9};
+    vector<int> arr{1,2,3,4,5,6};
+    vector<int> brr{2,4,6,8};
 
     findIntersection(arr, brr);
 
