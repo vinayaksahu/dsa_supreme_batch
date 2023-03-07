e77789e53b9c7424b92056e1551a9e11483ad27d
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-07 17:24:19 +0530

    maximum number

--- 3.Arrays Time & Space Comlexity/1.Array-I/5.MaximumNumber.cpp
+++ 3.Arrays Time & Space Comlexity/1.Array-I/5.MaximumNumber.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 int main() {
 
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int maxi = -1;//INT_MIN;
-    //Maximum number
-    for (int i = 0; i < size; i++){
-        if (arr[i] > maxi){
-            maxi = arr[i];
-        }
-    }
+    int arr[] = {1, 5, 4, 8 ,4 ,9, 6};
 
-    cout << "Maximum number: " << maxi;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > maxi)
+            maxi = arr[i];; 
+    }
+    
+    cout << "Maximum number: " << maxi << endl;
 
     return 0;
 }
