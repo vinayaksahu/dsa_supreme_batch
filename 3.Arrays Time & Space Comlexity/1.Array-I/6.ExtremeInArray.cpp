a93d307cfa70923e04c3f7568eb821dde27b20b6
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-07 18:07:15 +0530

    extreme in array

--- 3.Arrays Time & Space Comlexity/1.Array-I/6.ExtremeInArray.cpp
+++ 3.Arrays Time & Space Comlexity/1.Array-I/6.ExtremeInArray.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
-#include <limits.h>
+#include<iostream>
 using namespace std;
 
 int main() {
 
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr[] = {1,4,5,2,6,3,5};
     int size = sizeof(arr) / sizeof(arr[0]);
     int start = 0;
-    int end = size-1;
+    int end = size - 1;
 
-    while (start <= end) {
+    while(start < end) {
         if(start == end)
-            cout << arr[start];
-        else
-            cout << arr[start] <<" ";
-            cout << arr[end] <<" ";
+            cout << arr[start] << endl;
+        else    
+            cout << "(" << arr[start] << ", " <<arr[end] << ")"<< endl;
         start++;
         end--;
     }
-    
 
     return 0;
 }
