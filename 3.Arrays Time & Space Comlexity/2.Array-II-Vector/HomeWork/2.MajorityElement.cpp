2c737fb807975837072178b5f9f9d27df8597ca8
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-10 01:50:04 +0530

    row sum col sum

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/2.MajorityElement.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/2.MajorityElement.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 #include<vector>
+#include<limits.h>
 using namespace std;
 
 void findMajority(vector<int> arr) {
 
     int maxCount = 0;
-    int index = -1;
+    int index = INT_MIN;
     for (int i = 0; i < arr.size(); i++) {
-        int count = arr[i];
+        int count = 0;
         for (int j = 0; j < arr.size(); j++) {
-            if(arr[i] == arr[j]) 
+            if (arr[i] == arr[j]) 
                 count++;
         }
         if (count > maxCount) {
---
+++ 3.Arrays Time & Space Comlexity/3.Array-2D/1.rowSum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr[4][4] = {{1, 2, 3, 4},
+                     {5, 6, 7, 8},
+                     {1, 4, 7, 0},
+                     {9, 6, 3, 2}};
+    int row = 4;
+    int col = 4;
+
+    //print
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << arr[i][j] <<" ";
+        } cout << endl;
+    }
+ 
+    cout << "print row sum:"<<endl;
+    //print row sum
+    for (int i = 0; i < row; i++) {
+        int sum = 0;
+        for (int j = 0; j < col; j++) {
+            sum = sum + arr[i][j];
+            
+        } cout << "row " << i << " -> " << sum << endl;
+    }
+
+    return 0;
+}
