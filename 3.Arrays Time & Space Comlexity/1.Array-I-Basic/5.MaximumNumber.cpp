517c5beedfc8bbe18a0b2830359b980e0df7aecb
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-13 22:25:00 +0530

    maximum and minimum element of the array

--- 3.Arrays Time & Space Comlexity/1.Array-I-Basic/5.MaximumNumber.cpp
+++ 3.Arrays Time & Space Comlexity/1.Array-I-Basic/5.MaximumNumber.cpp
@@ -1,22 +1,41 @@
-#include <iostream>
-#include <limits.h>
+#include<iostream>
+#include<limits.h>
 using namespace std;
+
+int findMin(int arr[], int n) {
+    int mini = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if(mini > arr[i])
+            mini = arr[i];
+    }
+    
+    return mini;
+}
+
+int findMax(int arr[], int n) {
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if(maxi < arr[i])
+            maxi = arr[i];
+    }
+    
+    return maxi;
+}
 
 int main() {
 
-    int arr[] = {1, 5, 4, 8 ,4 ,9, 6, 9};
+    int arr[] = {1,4,5,6,2,3,9,8,7};
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int maxi = INT_MIN;
+    //find min and max
+    int mini = findMin(arr, size);
+    int maxi = findMax(arr, size);
 
-    for (int i = 0; i < size; i++) {
-        if (arr[i] > maxi) {
-            maxi = arr[i];
-        }
-    }
-    
-    cout << "Maximum number: " << maxi << endl;
+    cout << "Minimum of the array: " << mini << endl;
+    cout << "Maximum of the array: " << maxi << endl;
 
     return 0;
 }
