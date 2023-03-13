087a55e996bec91809d8ab99a17ad16cb1b51fb3
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-13 21:58:44 +0530

    count zero and one

--- 3.Arrays Time & Space Comlexity/1.Array-I-Basic/4.countZeroOne.cpp
+++ 3.Arrays Time & Space Comlexity/1.Array-I-Basic/4.countZeroOne.cpp
@@ -1,25 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main() {
 
-    int arr[] = {0, 0, 1, 0, 1, 1, 1, 0, 0};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {0,1,0,1,1,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0,1,1,0,1};
+
+    int n = sizeof(arr) / sizeof(int);
+
+    //count zero's and one's
+
     int countZero = 0;
     int countOne = 0;
 
-    //Linear Search
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == 0){
+    for (int i = 0; i < n; i++) {
+        if(arr[i] == 0)
             countZero++;
-        }
-        if (arr[i] == 1){
+        if(arr[i] == 1)
             countOne++;
-        }
     }
 
-    cout << "0's: " << countZero << endl;
-    cout << "1's: " << countOne << endl;
+    cout << "Zero's: " << countZero << endl;
+
+    cout << "One's: " << countOne << endl;
 
     return 0;
 }
