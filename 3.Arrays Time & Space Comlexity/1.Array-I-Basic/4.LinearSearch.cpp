cea0653bda91fba47cde34b53df31c4afa7e4a4b
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-13 22:09:36 +0530

    linear search

--- 3.Arrays Time & Space Comlexity/1.Array-I-Basic/4.LinearSearch.cpp
+++ 3.Arrays Time & Space Comlexity/1.Array-I-Basic/4.LinearSearch.cpp
@@ -1,29 +1,27 @@
 #include<iostream>
 using namespace std;
 
+void findKey(int arr[], int n, int k) {
+
+    for (int i = 0; i < n; i++) {
+        if(arr[i] == k) {
+            cout << "Key found at index: " << i;
+            return;
+        }
+    }
+
+    cout << "Key not found.";
+}
+
 int main() {
 
     int arr[] = {1, 2, 4, 5, 7, 8, 9 ,6 ,3};
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int key = 7;
+    int key = 17;
 
-    int flag = 0;
-    int i;
-    for (i = 0; i < size; i++) {
-        if (key == arr[i]) {
-            flag = 1;
-            break;
-        }
-    }
-
-    if (flag) {
-        cout << "Key found at index: " << i << endl;
-    }
-    else {
-        cout << "Key not found." << endl;
-    }
+    findKey(arr, size, key);
     
     return 0;
 }
