318f55537209e76166772cb5eb248f8012c586fc
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 11:28:42 +0530

    common elements of 2 arrays

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/3.Intersection.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/3.Intersection.cpp
@@ -7,33 +7,35 @@ void printArray(vector<int> ans) {
 
     for(auto val : ans) {
         cout << val << " ";
-    } cout << endl;
+    }
 }
 
-void findIntersection (vector<int> arr, vector<int> brr) {
+void findIntersection(vector<int> arr, vector<int> brr) {
 
     vector<int> ans;
 
-    //every element of arr will traverse brr
     for (int i = 0; i < arr.size(); i++) {
         int element = arr[i];
-        for (int j = 0; j < brr.size(); j++) {
+        for(int j = 0; j < brr.size(); j++) {
             if (element == brr[j]) {
                 ans.push_back(brr[j]);
-                brr[j] = INT_MIN; //break;
+                break;
+                //brr[j] = INT_MIN;
             }
         }
     }
+    
 
-    //print array
+    //print 
     printArray(ans);
 }
 
 int main() {
 
-    vector<int> arr{1,2,3,4,5,6};
-    vector<int> brr{2,4,6,8};
+    vector<int> arr{1,3,5,7,9};
+    vector<int> brr{2,4,6,8,1};
 
+    //Intersection (common elements) of arrays
     findIntersection(arr, brr);
 
     return 0;
