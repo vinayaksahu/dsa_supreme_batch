5a90b64960938990f095602b68e39ba307a3b51b
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 16:24:17 +0530

    majority element of the array

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/2.MajorityElement.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/2.MajorityElement.cpp
@@ -7,29 +7,29 @@ void findMajority(vector<int> arr) {
 
     int maxCount = 0;
     int index = INT_MIN;
+
+    //traverse all elements of the array to find maxCount
     for (int i = 0; i < arr.size(); i++) {
         int count = 0;
-        for (int j = 0; j < arr.size(); j++) {
-            if (arr[i] == arr[j]) 
-                count++;
-        }
+        for (int j = 0; j < arr.size(); j++)
+            if (arr[i] == arr[j])
+                count++;   
         if (count > maxCount) {
             maxCount = count;
             index = i;
         }
     }
 
-    if (maxCount > arr.size()/2) {
-        cout << arr[index] << endl;
-    }
-    else {
-        cout << "No majority element.";
-    }
+    if(maxCount > arr.size()/2)
+        cout << "Majority element is " << arr[index];
+    else
+        cout << "No majority element";
+    
 }
 
 int main() {
 
-    vector<int> arr{1,2,3,1,2,3,0,3,3,3,3,3,3,3};
+    vector<int> arr{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,3,3,0,1,2,3,0,2,1,3};
 
     findMajority(arr);
 
