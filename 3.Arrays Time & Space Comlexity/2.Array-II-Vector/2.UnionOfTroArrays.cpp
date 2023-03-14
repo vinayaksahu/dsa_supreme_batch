88c317c394e0c2baa5834d705e81a7d2ab1c1faa
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 11:12:54 +0530

    union of array

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/2.UnionOfTroArrays.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/2.UnionOfTroArrays.cpp
@@ -6,32 +6,33 @@ void printArray(vector<int> ans) {
 
     for(auto val : ans) {
         cout << val << " ";
-    } cout << endl;
+    }
 }
 
-void findUnion(vector<int> arr, vector<int> brr) {
+void findUnion(vector<int> a, vector<int> b) {
 
     vector<int> ans;
 
-    //insert elements of arr to ans
-    for (int i = 0; i < arr.size(); i++) {
-        ans.push_back(arr[i]);
+    //insert elements of array 'a' to array 'ans'
+    for (int i = 0; i < a.size(); i++) {
+        ans.push_back(a[i]);
     }
-    //again insert brr elements to ans
-    for (int i = 0; i < brr.size(); i++) {
-        ans.push_back(brr[i]);
-    }
-    
-    //print array
+
+    //insert elements of array 'b' to array 'ans'
+    for (int i = 0; i < b.size(); i++) {
+        ans.push_back(b[i]);
+    }   
+
+    //print
     printArray(ans);
 }
 
 int main() {
 
     vector<int> arr{1,3,5,7};
-    vector<int> brr{2,4,6,7,8};
+    vector<int> brr{2,4,6,8};
 
+    //find union of the array
     findUnion(arr, brr);
-
     return 0;
 }
