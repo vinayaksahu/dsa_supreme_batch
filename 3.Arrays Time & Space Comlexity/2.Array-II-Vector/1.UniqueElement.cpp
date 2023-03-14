4e1ace082453f7cf5a75516254532f5973fcb91c
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 11:03:20 +0530

    unique element

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/1.UniqueElement.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/1.UniqueElement.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 int findUnique(vector<int> arr) {
-
     int ans = 0;
 
     for (int i = 0; i < arr.size(); i++) {
         ans = ans ^ arr[i];
     }
-
+    
     return ans;
 }
 
 int main() {
 
-    vector<int> arr{1,4,7,2,5,8,3,6,9,1,4,7,2,5,8,3,6};
+    vector<int> arr{1,2,3,4,5,6,7,8,9,1,2,3,4,6,7,8,9};
 
-    int uniqueElement = findUnique(arr);        
+    int uniqueElement = findUnique(arr);
 
-    cout << "Unique element of the array arr is: " << uniqueElement;
+    cout << "Unique element: " << uniqueElement;
 
     return 0;
 }
