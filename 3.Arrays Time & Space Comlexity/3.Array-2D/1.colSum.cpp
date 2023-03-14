65295c595d001984f234dfbb937990f1733b08ce
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 19:12:46 +0530

    print row and col sum

--- 3.Arrays Time & Space Comlexity/3.Array-2D/1.colSum.cpp
+++ 3.Arrays Time & Space Comlexity/3.Array-2D/1.colSum.cpp
@@ -1,32 +1,46 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+void printRowSum(int arr[][3], int m, int n) {
 
-    int arr[4][4] = {{1, 2, 3, 4},
-                     {5, 6, 7, 8},
-                     {1, 4, 7, 0},
-                     {9, 6, 3, 2}};
-    int row = 4;
-    int col = 4;
+    for (int i = 0; i < m; i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum = sum + arr[i][j];
+        }   cout << "Row " << i << " :" << sum << endl;
+    }
+}
 
-    //print
+void printColSum(int arr[][3], int m, int n) {
+
+    for (int i = 0; i < m; i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum = sum + arr[j][i];
+        }   cout << "Col " << i << " :" << sum << endl;
+    }
+}
+
+
+int main () {
+
+    int arr[3][3] = {{1, 2, 3},
+                     {4, 5, 6},
+                     {7, 8, 9}};
+
+    int row = 3;
+    int col = 3;
+                    
+    //given element
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
-            cout << arr[i][j] <<" ";
+            cout << arr[i][j] <<" ";            
         } cout << endl;
     }
 
-    cout << "print row sum:"<<endl;
     //print row sum
-    for (int i = 0; i < row; i++) {
-        int sum = 0;
-        int j;
-        for (j = 0; j < col; j++) {
-            sum = sum + arr[j][i];
-            
-        } cout << "col " << i << " -> " << sum << endl;
-    }
+    printRowSum(arr, row, col);
 
-    return 0;
+    //print column sum
+    printColSum(arr, row, col); 
 }
