2ffd633b9451cb3cf7c748cd44e796a4994eb4f9
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 11:42:23 +0530

    pair sum

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/4.PairSum.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/4.PairSum.cpp
@@ -2,25 +2,27 @@
 #include<vector>
 using namespace std;
 
-void pairSum(vector<int> arr, int sum) {
+void findPairSum(vector<int> arr, int sum) {
 
     for (int i = 0; i < arr.size(); i++) {
-        int element = arr[i];
-        //for every element, will travese on next element
+        int element1 = arr[i];
         for (int j = i+1; j < arr.size(); j++) {
-            if(sum == element + arr[j])
-                cout << element <<", " << arr[j] <<endl;
-        }   
+            int element2 = arr[j];
+            if (sum == element1 + element2) {
+                cout << element1 << " " << element2 << endl;           
+            }
+        }
     }
 }
 
 int main() {
-    
+
     vector<int> arr{1,2,3,4,5,6,7,8,9};
 
-    int sum = 10;
+    //find ki kitne pairs ka sum 13 hai
+    int sum = 13;
 
-    pairSum(arr, sum);    
+    findPairSum(arr, sum);
 
     return 0;
 }
