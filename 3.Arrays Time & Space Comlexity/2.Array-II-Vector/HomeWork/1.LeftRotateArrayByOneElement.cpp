bb47db75404c53c52f4a4eea27552704c92862b3
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-14 12:59:32 +0530

    rotate array by one element

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/1.LeftRotateArrayByOneElement.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/1.LeftRotateArrayByOneElement.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main() {
-
-    int arr[] = {1,2,3,4,5,6,7,8,9};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    //Left rotate by one element
-    int first = arr[0]; //store first element at first variable
-/*     for (int i = 1; i < n; i++) {
-        arr[i-1] = arr[i]; //left shift elements by one elemnt
-    } */
-    for (int i = 0; i < n-1; i++) {
-        arr[i] = arr[i+1];
+void printArray(vector<int> arr) {
+    for(auto val : arr) {
+        cout << val << " ";
     }
-    
-    arr[n-1] = first; // store 1st element(first) tp 
+}
 
-    //print array
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+void rotateArray(vector<int> arr, int n) {
+    int firstElement = arr[0];
+    for (int i = 1; i < n; i++) {
+        arr[i-1] = arr[i];   
     }
-    
+    arr[n-1] = firstElement;
+
+    //print
+    printArray(arr);
+}
+
+int main () {
+
+    vector<int> arr{1,2,3,4,5,6,7,8,9};
+    int size = arr.size() - 1;
+    //rotate array by one element
+    rotateArray(arr, size);
+
     return 0;
 }
