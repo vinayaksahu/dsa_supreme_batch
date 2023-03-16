311d508b6b6659b12fa18631bf63dc70119c4524
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-16 23:10:46 +0530

    binary search practicinf

--- 4.Searching and Sorting/Searching and Sorting -I/1.BinarySearch.cpp
+++ 4.Searching and Sorting/Searching and Sorting -I/1.BinarySearch.cpp
@@ -3,58 +3,56 @@
 #include<algorithm>
 using namespace std;
 
-int binarySearch(vector<int> arr, int key) {
+int binary_search(vector<int> arr, int key) {
 
     int s = 0;
     int e = arr.size()-1;
     int mid = s + (e - s) / 2;
+
     while(s <= e) {
-        //agr key mil gya
-        if (arr[mid] == key) {
+
+        //found
+        if(key == arr[mid])
             return mid;
-        }
-        //nhi mila - left search
-        else if(arr[mid] > key) {
+        //left search
+        else if(key < arr[mid])
             e = mid - 1;
-        }
-        //nhi mila - right search
-        else if(arr[mid] < key) {
+        //right search
+        else //if(key > arr[mid])
             s = mid + 1;
-        }
+        //update mid
         mid = s + (e - s) / 2;
     }
-
     return -1;
 }
 
-int main() {
+int main(){
+    //array - binary search inbuild
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int size = 9;
+    int arrTgt = 8;
+    if(binary_search(arr, arr + size, arrTgt))
+        cout << "Array: Found" << endl;
+    else
+        cout << "Array: Not found" << endl;
     
-    //Binary Search using inbuild function
-    //for array
-    int brr[] = {1,2,3,4,5,6,7};
-    int size = 7;
-    int brrKey = 7;
-    if(binary_search(brr, brr + size, brrKey))
-        cout << "array:Found" << endl;
+    //vector - binary search inbuild
+    vector<int> vrr{1,2,3,4,5,6,7,8,9};
+    int vTgt = 7;
+    bool vIdx = binary_search(vrr.begin(), vrr.end(), vTgt);
+    if(vIdx)
+        cout << "Vector: Found" << endl;
+    else
+        cout << "Vector: Not found" << endl;
+
+    //Fuction - create yourself binary search function
+    vector<int> fnr{1,2,3,4,5,6,7,8,9};
+    int fnTgt = 9;
+    int fnIdx = binary_search(fnr, fnTgt);
+    if(fnIdx)
+        cout << "Function: Found" << endl;
     else    
-        cout << "array:Not found" << endl;
-
-    //for vector
-    vector<int> arr{1,2,3,4,5,6,7,8,9};
-    int arrKey = 8;
-    if (binary_search(arr.begin(), arr.end(), arrKey))
-        cout << "vector:Found" << endl;
-    else
-        cout << "vector:Not found" << endl;
-
-    //Binary Search using function
-    int key = 8;
-    int index = binarySearch(arr, key);
-
-    if (index == -1)
-        cout << "Not found.";
-    else
-        cout << "Found at index: " << index;
+        cout << "Function: Not found" << endl;
 
     return 0;
 }
