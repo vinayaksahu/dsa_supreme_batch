82eb26c177437548cbedf125337f8e708c7ad791
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-17 01:25:54 +0530

    1st and last occurance

--- 4.Searching and Sorting/Searching and Sorting -I/2.FirstLastOccurance.cpp
+++ 4.Searching and Sorting/Searching and Sorting -I/2.FirstLastOccurance.cpp
@@ -3,35 +3,54 @@
 using namespace std;
 
 int findFirstOcc(vector<int> arr, int target) {
-
+    int ans = -1;
     int s = 0;
     int e = arr.size()-1;
     int mid = s + (e - s) / 2;
-    int ans = -1;
-    while (s <= e) {
+    while(s <= e) {
+        //agr target mil gya
         if(arr[mid] == target) {
-            ans = mid;
-            e = mid - 1;
+            ans = mid; //ans store kra do
+            e = mid - 1; //ab left me search krna h gr 1st occurance hai to
         }
-        else if(arr[mid] > target) {
+        else if(arr[mid] > target) //left me search krna
+            e = mid - 1;
+        else if(arr[mid] < target) //right me search krna
             s = mid + 1;
-        }
-        else if(arr[mid] > target) {
-            e = mid - 1;
-        }
-        mid = s + (e - s) / 2;
+        mid = s + (e - s) / 2; //update mid
     }
-    
+    return ans;
+}
 
+int findLastOcc(vector<int> arr, int target) {
+    int ans = -1;
+    int s = 0;
+    int e = arr.size()-1;
+    int mid = s + (e - s) / 2;
+    while(s <= e) {
+        //agr target mil gya
+        if(arr[mid] == target) {
+            ans = mid; //ans store kra do
+            s = mid + 1; //ab right me search krna h agr last occurance hai to
+        }
+        else if(arr[mid] > target) //left me search krna
+            e = mid - 1;
+        else if(arr[mid] < target) //right me search krna
+            s = mid + 1;
+        mid = s + (e - s) / 2; //update mid
+    }
     return ans;
 }
 
 int main () {
 
-    vector<int> arr{4,4,4,4,4,4,4,4,4,4,4,4,4};
+    vector<int> arr{4,4,4,4,4};
     int target = 4;
-    int firstOcc = findFirstOcc(arr, target);
-    cout << "First occurance index: " << firstOcc;
+    int firstOccIdx = findFirstOcc(arr, target);
+    cout << "First occurance index: " << firstOccIdx << endl;
+
+    int lasttOccIdx = findLastOcc(arr, target);
+    cout << "Last occurance index: " << lasttOccIdx << endl;
 
     return 0;
 }
