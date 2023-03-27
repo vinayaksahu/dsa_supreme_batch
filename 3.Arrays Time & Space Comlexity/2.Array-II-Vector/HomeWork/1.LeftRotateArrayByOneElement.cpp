63a4e115d7807c09d09c876fa6242e3bd0a1f22e
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-28 02:00:53 +0530

    left rotate array

--- 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/1.LeftRotateArrayByOneElement.cpp
+++ 3.Arrays Time & Space Comlexity/2.Array-II-Vector/HomeWork/1.LeftRotateArrayByOneElement.cpp
@@ -9,9 +9,10 @@ void printArray(vector<int> arr) {
 }
 
 void rotateArray(vector<int> arr, int n) {
+
     int firstElement = arr[0];
     for (int i = 1; i < n; i++) {
-        arr[i-1] = arr[i];   
+        arr[i-1] = arr[i];
     }
     arr[n-1] = firstElement;
 
@@ -22,7 +23,7 @@ void rotateArray(vector<int> arr, int n) {
 int main () {
 
     vector<int> arr{1,2,3,4,5,6,7,8,9};
-    int size = arr.size() - 1;
+    int size = arr.size();
     //rotate array by one element
     rotateArray(arr, size);
 
