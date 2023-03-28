e821659af53bf382e1d31a8d7bfc51c6c55c2704
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-28 23:37:25 +0530

    selection sort

---
+++ 4.Searching and Sorting/5.Sorting Technique/1.SelectionSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printArray(vector<int> arr) {
+
+    for (auto &val : arr)
+        cout << val << " ";
+    cout << endl;
+}
+
+void selectionSort(vector<int> arr) {
+
+    int n = arr.size();
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        swap(arr[min_idx], arr[i]);
+    }
+
+    //print
+    printArray(arr);
+}
+ 
+int main() {
+
+    vector<int> arr{1,5,9,4,2,3,6,5,8};
+
+    //sort
+    selectionSort(arr);
+
+    return 0;
+}
