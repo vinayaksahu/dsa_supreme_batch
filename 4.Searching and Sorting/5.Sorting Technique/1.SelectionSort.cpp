9f2b3f4c2e2afeeae5c8c96c89f87c4ecc86ad6b
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-29 16:35:30 +0530

    selection sort

--- 4.Searching and Sorting/5.Sorting Technique/1.SelectionSort.cpp
+++ 4.Searching and Sorting/5.Sorting Technique/1.SelectionSort.cpp
@@ -14,13 +14,14 @@ void selectionSort(vector<int> arr) {
     int n = arr.size();
     for (int i = 0; i < n-1; i++) {
         int min_idx = i;
-        for (int j = i+1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
+        for (int j = 0; j < n; j++) {
+            if (arr[min_idx] > arr[j])
                 min_idx = j;
         }
         swap (arr[min_idx], arr[i]);
     }
-    //print 
+    
+    //print
     printArray(arr);
 }
  
---
+++ 4.Searching and Sorting/5.Sorting Technique/2.BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printArray(vector<int> arr) {
+    for (auto &val : arr)
+        cout << val << " ";
+    cout << endl;
+}
+
+void bubbleSort(vector<int> arr) {
+    int n = arr.size();
+    for (int i = 0; i < n-1; i++)
+        for (int j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1])
+                swap (arr[j], arr[j+1]);
+
+    //print
+    printArray(arr);
+}
+
+int main () {
+
+    vector<int> arr{1,4,5,2,3,6,9,8,7};
+
+    //sort
+    bubbleSort(arr);
+
+    return 0;
+}
