38d20bb433661c9e7573e655955451755bad32d3
Vinayaksahu <vinayaksahu3@gmail.com>
2023-03-31 23:18:06 +0530

    selection sort

--- 4.Searching and Sorting/5.Sorting Technique/1.SelectionSort.cpp
+++ 4.Searching and Sorting/5.Sorting Technique/1.SelectionSort.cpp
@@ -15,12 +15,12 @@ void selectionSort(vector<int> arr) {
     for (int i = 0; i < n-1; i++) {
         int min_idx = i;
         for (int j = i+1; j < n; j++) {
-            if (arr[min_idx] > arr[j])
+            if (arr[j] < arr[min_idx])
                 min_idx = j;
         }
         swap (arr[min_idx], arr[i]);
     }
-    
+
     //print
     printArray(arr);
 }
