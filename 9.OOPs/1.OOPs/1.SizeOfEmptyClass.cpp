2475c5deed3d1093276f646069deae7263a0ed23
Vinayaksahu <vinayaksahu3@gmail.com>
2023-04-03 02:34:59 +0530

    size of empty class

---
+++ 9.OOPs/1.OOPs/1.SizeOfEmptyClass.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Animal {
+
+int a;
+char ch;
+bool b;
+
+};
+
+int main () {
+
+    //cout << "Size of empty class: " << sizeof(Animal);
+    cout << "Size of Animal class: " << sizeof(Animal);
+
+    return 0;
+}
