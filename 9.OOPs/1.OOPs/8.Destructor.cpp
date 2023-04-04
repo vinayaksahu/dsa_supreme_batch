1638770db080603e064d38cad8782f8a8e6594d0
Vinayaksahu <vinayaksahu3@gmail.com>
2023-04-04 21:47:52 +0530

    OOPs class -1 complete

---
+++ 9.OOPs/1.OOPs/8.Destructor.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Box {
+
+    public:
+
+        ~Box() {
+            cout << "Inside destructor" << endl;
+        }
+
+};
+
+int main() {
+
+    Box b1;
+
+    Box *b2 = new Box;
+    delete b2;
+
+    return 0;
+}
