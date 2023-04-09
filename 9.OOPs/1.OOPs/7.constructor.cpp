5c42467be10e012899cedad903eb8ec0f5bfab2c
Vinayaksahu <vinayaksahu3@gmail.com>
2023-04-09 22:54:50 +0530

    contructor

--- 9.OOPs/1.OOPs/7.constructor.cpp
+++ 9.OOPs/1.OOPs/7.constructor.cpp
@@ -4,9 +4,71 @@ using namespace std;
 class Animal {
 
     private:
+        int weigth;
+    
+    public:
+        int age;
+        string type;
+
+        //default constructor
+        Animal() {
+            cout << "Default constructor called." << endl << endl;
+        }
+
+        //paramterized constructor
+        Animal(int age) { // 1 parameter
+            this->age = age;
+            cout << "Age of animal: " << this->age << endl;
+            cout << "Parameterized constructor called with 1 parameter." << endl << endl;
+        }
+ 
+        Animal(int age, int weight, string type) { //3 parameter
+            this->age = age;
+            this->weigth = weigth;
+            this->type = type;
+            cout << "Age: " << this->age << endl;
+            cout << "Weight: " << this->weigth << endl;
+            cout << "Type: " << this->type << endl;
+            cout << "Parameterized constructor called with 3 parameter." << endl << endl;
+        }
+
+        //copy constructor
+        Animal(Animal &dog) {
+            this->age = dog.age;
+            this->type = dog.type;
+            cout << this->type << endl;
+            cout << "Copy constructor called." << endl;
+        }
+};
+
+int main() {
+
+    Animal cow; //default constructor
+
+    Animal cat(5); //paramterized constructor - 1 parameter
+    Animal dog(10, 20, "puppy"); //paramterized constructor - 3 parameter <= static
+    Animal* doggy = new Animal(11,19, "pupp");
+
+    Animal bulldog(dog); //copy constructor <= static
+    Animal* liondog = new Animal(dog); //copy contructor <= dynamic
+
+    return 0;
+}
+
+
+
+
+/* 
+#include<iostream>
+using namespace std;
+
+class Animal {
+
+     private:
         int weight;
 
     public:
+        int weight;
         int age;
         string type;
 
@@ -15,23 +77,23 @@ class Animal {
             this->weight = 0;
             this->age = 0;
             this->type = "";
-            cout << "Default Contructor called." << endl;
+            cout << "Default Contructor called." << endl << endl;
         }
 
         //Parameterised constructor
         Animal(string type) { // 1 parameter passed
             this->type = type;
             cout << "Parameterised Contructor with 1 parameter." << endl;
-            cout << "type of the animal: " << type << endl << endl;
+            cout << "type of the animal: " << this->type << endl << endl;
         }
         Animal(int weight, int age, string type) { // 3 papameter passed
             this->weight = weight;
             this->age = age;
             this->type = type;
             cout << "Parameterised Contructor with 3 parameter." << endl;
-            cout << "weight of the animal: " << weight << endl;
-            cout << "age of the animal: " << weight << endl;
-            cout << "type of the animal: " << type << endl << endl;
+            cout << "weight of the animal: " << this->weight << endl;
+            cout << "age of the animal: " << this->age << endl;
+            cout << "type of the animal: " << this->type << endl << endl;
         }
 
         //Copy constructor -> camle = elephant
@@ -41,7 +103,7 @@ class Animal {
             this->type = elephant.type;
             cout << "Copy Contructor." << endl;
             cout << "weight of the animal: " << weight << endl;
-            cout << "age of the animal: " << weight << endl;
+            cout << "age of the animal: " << age << endl;
             cout << "type of the animal: " << type << endl << endl;
         }
 };
@@ -49,13 +111,13 @@ class Animal {
 int main() {
 
     Animal dog; //Default contructor
+
     Animal cat("cat"); //Parameterised contructor 1 parameter
+    Animal cow(500, 20, "cow"); //Parameterised contructor 3 parameter <- static
+    Animal* elephant = new Animal(10000, 50, "elephant"); //Parameterised contructor 3 parameter <- Dynamic
 
-    //Animal elephant(5000, 50, "Asian"); //Parameterised contructor 3 parameter <- static
-    Animal* elephant = new Animal(5000, 50, "Asian"); //Parameterised contructor 3 parameter <- Dynamic
-
-    Animal camle(*elephant); //copy constructor 
+    Animal bigElephant(*elephant); //copy constructor 
     //Animal camle = elephant; //copy constructor -> another way to declare copy contructor
 
     return 0;
-}
+} */
