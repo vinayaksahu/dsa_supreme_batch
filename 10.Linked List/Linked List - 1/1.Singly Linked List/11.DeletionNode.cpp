b33c44bebea556525fca289fb912944e4d152f98
Vinayaksahu <vinayaksahu3@gmail.com>
2023-04-15 16:56:18 +0530

    Linked list class 1 revision complete

--- 10.Linked List/Linked List - 1/1.Singly Linked List/11.DeletionNode.cpp
+++ 10.Linked List/Linked List - 1/1.Singly Linked List/11.DeletionNode.cpp
@@ -8,161 +8,136 @@ class Node {
         Node(int data) {
             this->data = data;
             this->next = NULL;
-        }  
-        ~Node() {
-            cout << "Node with value " << this->next << " deleted " << endl;;
         }
 };
 
-int findLength(Node* head) {
+void print(Node* &head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } cout << endl;
+}
+
+int findLength(Node* &head) {
     Node* temp = head;
     int len = 0;
-    while(temp != NULL) {
+    while (temp != NULL) {
         temp = temp->next;
         len++;
     }
     return len;
 }
 
-void print(Node* &head) {
-    Node* temp = head;
-    while(temp != NULL) {
-        cout << temp->data << " ";
-        temp = temp->next;
-    } cout << endl;
-}
-
 void insertAtHead(Node* &head, Node* &tail, int data) {
-    //check for Empty LL
-    if(head == NULL) {
-        //create node
-        Node* newnode = new Node(data);
-        //update head
-        head = newnode;
-        //update tail
-        tail = newnode;
+
+    if (head == NULL) { //check for empty LL
+        Node* newnode = new Node(data); //create node
+        head = newnode; //update head
+        tail = newnode; //& tail
         return;
     }
 
-    //create node
-    Node* newnode = new Node(data);
-    //connect newnode to head
-    newnode->next = head;
-    //update head
-    head = newnode;
+    Node* newnode = new Node(data); //create newnode
+    newnode->next = head; //connect newnode to head
+    head = newnode; // update head
 }
 
 void insertAtTail(Node* &head, Node* &tail, int data) {
-    //check for empty LL
-    if (head == NULL) {
-        //create new node
-        Node* newnode = new Node(data);
-        //update head
-        head = newnode;
-        //update tail
-        tail = newnode;
+    
+    if (head == NULL) { //check for empty LL
+        Node* newnode = new Node(data); //create newnode
+        head = newnode; //update head
+        tail = newnode; //& tail
         return;
     }
-    //create newnode
-    Node* newnode = new Node(data);
-    //connect tail to newnode
-    tail->next = newnode;
-    //update tail
-    tail = newnode;
+
+    Node* newnode = new Node(data); //create newnode
+    tail->next = newnode; //connet tail to newnode
+    tail = newnode; //update tail
 }
 
 void insertAtPosition(Node* &head, Node* &tail, int data, int position) {
-    //check for empty LL
-    if (head == NULL) {
-        //create node
-        Node* newnode = new Node(data);
-        //update head & tail
-        head = newnode;
-        tail = newnode;
-        return;
-    }
     
-    //insert at the head
-    if(position == 0) {
+    if (head == NULL) { //check for empty LL
+        Node* newnode = new Node(data); //create newnode
+        head = newnode; //update head
+        tail = newnode; //&tail
+        return;
+    }    
+
+    //insert at head
+    if (position == 0) {
         insertAtHead(head, tail, data);
         return;
     }
 
-    //insert at the tail
+    //insert at tail
     int len = findLength(head);
-    if(position >= len) {
+    if (position >= len) {
         insertAtTail(head, tail, data);
         return;
     }
 
-    //insert at middle - find 1st previous and current node
-    Node* prev = head;
+    //insert at middle
+    Node* prev = head; //find previous node
     int i = 1;
-    while(i < position) {
-        prev = prev->next; 
+    while (i < position) {
+        prev = prev->next;
         i++;
-    }
-    Node* curr = prev->next;
+    } Node* curr = prev->next; //& current node
+    
+    Node* newnode = new Node(data); //create newnode
+    newnode->next = curr; //connect newnode to curr
+    prev->next = newnode; //connect prev to newnode    
 
-    //create newnode    
-    Node* newnode = new Node(data);
-    //connect newnode to current node
-    newnode->next = curr;
-    //connect previous node to currrent node 
-    prev->next = newnode;
 }
 
-void deleteNode(Node* &head, Node* &tail, int position) {
-    //check for empty
-    if (head == NULL) {
-        cout << "Linked list is empty" << endl;
-        return; 
-    }
-
-    //delete first node
-    if (position == 1) {
-        Node* temp = head; //head ko temp nam rkho
-        head = head->next; //head k next ko head bnao
-        temp->next = NULL; //temp ko null kr do
-        delete temp; //delete kr do
+void deletion(Node* &head, Node* &tail, int position) {
+    
+    if (head == NULL) { //check for empty LL
+        cout << "Linked List is Empty." << endl;
         return;
     }
-    
 
-    //delete last node
+    //delete head
+    if (position == 1) {
+        Node* temp = head; //name temp to head
+        head = head->next; //update head
+        temp->next = NULL; //assign temp to NULL
+        delete temp;
+    }
+    
+    //delete tail
     int len = findLength(head);
     if (position == len) {
-        //find prev of tail
-        int i = 1;
+        //find previous node of tail node
         Node* prev = head;
-        while (i < position-1) {
+        int i = 1;
+        while (i < position - 1) {
             prev = prev->next;
             i++;
         }
-
-        //prev->next ko null kr diya
-        prev->next = NULL;
-        //update tail
-        Node* temp = tail;
-        tail = prev;
-        //delete last node
-        delete temp;
+        Node* temp = tail; //name temp to tail
+        tail = prev; //update tail
+        tail->next = NULL; //assign tail to NULL
+        delete temp; //delete temp
         return;
     }
 
-    //delete middle node
-    //  find prev and next
-    int i = 1;
-    Node* prev = head;
-    while(i < position - 1) {
-        prev = prev->next;
-        i++;
-    } Node* curr = prev->next;
+    //delete middle
     
-    //connect previous to current ka next
-    prev->next = curr->next;
-    //delete current node
-    delete curr;
+    Node* prev = head;
+    int i = 1;
+    while(i < position - 1) {
+        prev = prev->next; //find previous 
+        i++;
+    } Node* curr = prev->next; //& current node
+
+    prev->next = curr->next; //connect previous to next node of current node
+    curr->next = NULL; //assign current node to NULL
+    delete curr; //delete current node
+
 }
 
 int main() {
@@ -170,54 +145,42 @@ int main() {
     Node* head = NULL;
     Node* tail = NULL;
 
-    //insert at head
+    //insert head
     insertAtHead(head, tail, 20);
     insertAtHead(head, tail, 10);
 
-    //insert at tail
+    //insert tail
     insertAtTail(head, tail, 1);
     insertAtTail(head, tail, 2);
 
     //print
     print(head);
 
-    //print head & tail
-    cout << "Head: " << head->data << endl;
-    cout << "Tail: " << tail->data << endl;
-
-    //lenght of above LL
-    cout << "Length: "<< findLength(head) << endl 
+    //length
+    int len = findLength(head);
+    cout << "Length: " << len << endl
          << endl;
 
-/******************************************************/
+/*******************************************************/
 
     //insert at position
-    int position = 0;
-    cout << "Insert Position: " << position << endl;
-    insertAtPosition(head, tail, 500, position);
+    int position = 6;
+    int data = 500;
+    cout << "Position to insert data: " << position << endl;
+    insertAtPosition(head, tail, data, position);
 
     //print
     print(head);
 
-    //print head & tail
-    cout << "Head: " << head->data << endl;
-    cout << "Tail: " << tail->data << endl
-         << endl;
+/*******************************************************/
 
-/******************************************************/
-
-    //delete node
-    position = 3;
-    cout << "Delete: " << position << endl;
-    deleteNode(head, tail, position);
+    //deletion
+    int deleteNode = 3;
+    cout << "Delete Node: " << deleteNode << endl;
+    deletion(head, tail, deleteNode);
 
     //print
     print(head);
 
-    //print head & tail
-    cout << "Head: " << head->data << endl;
-    cout << "Tail: " << tail->data << endl;
-
-    
     return 0;
 }
