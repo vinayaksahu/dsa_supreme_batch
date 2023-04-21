2af091024aacce3eed894e60945b9d0f70a437c2
Vinayaksahu <vinayaksahu3@gmail.com>
2023-04-21 21:42:08 +0530

    prsent or not iterative method

--- 7.Recursion & Back Tracking/1.Recursion/Home Work/9.FindKeyPresentOrNot.cpp
+++ 7.Recursion & Back Tracking/1.Recursion/Home Work/9.FindKeyPresentOrNot.cpp
@@ -2,13 +2,29 @@
 #include<vector>
 using namespace std;
 
+bool findKey(vector<int> arr, int n, int key) {
+
+    for (int i = 0; i < n; i++) {
+        if(arr[i] == key)
+            return true;
+    }
+    return false;
+}
+
 int main() {
 
     vector<int> arr = {1,4,8,5,9,6,3,2,7};
 
     int size = arr.size();
 
-    
+    int key = 0;
+
+    int present = findKey(arr, size, key);
+
+    if(present)
+        cout << "Present.";
+    else    
+        cout << "Not present.";
 
     return 0;
 }
