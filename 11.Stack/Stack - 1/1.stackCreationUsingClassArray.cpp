6f60efc3bb8746a1c9cc1ae454903a1252837757
Vinayaksahu <vinayaksahu3@gmail.com>
2023-05-08 22:48:57 +0530

    stack creation using array oops

--- 11.Stack/Stack - 1/1.stackCreationUsingClassArray.cpp
+++ 11.Stack/Stack - 1/1.stackCreationUsingClassArray.cpp
@@ -1,83 +1,85 @@
 #include<iostream>
-#include<stack>
 using namespace std;
 
 class Stack {
     //properties
     private:
-        int *arr;
-        int top;
+        int* arr;
         int size;
-
-    public:
-    //constructor
-    Stack(int size) {
-        arr = new int(size);
-        this->size = size;
-        top = -1;
-    }
-
-    //behaviour / functions
-    void push(int data) {
-        if(size - top > 1) {
-            top++;
-            arr[top] = data;
-        } else {
-            cout << "Stack overflow." << endl;
+        int top;
+    
+    public: 
+        //constructor
+        Stack(int size) {
+            arr = new int[top];
+            this->size = size;
+            top = -1;
         }
-    }
+        
+        //behaviour / functions
+        //push
+        void push(int data) {
+            if(size - top > 1) {
+                top++;
+                arr[top] = data;
+            }
+            else
+                cout << "Stack overflow." << endl;
+        }
 
-    void pop() {
-        if(top == -1)
-            cout << "Stack underflow." << endl;
-        else
-            top--;
-    }
+        //pop
+        void pop() {
+            if(top == -1)
+                cout << "Stack underflow." << endl;
+            else 
+                top--;
+        }
 
-    int topp() {
-        if(top == -1)
-            cout << "There is not element in stack." << endl;
-        else    
-            return arr[top];
-    }
+        //top
+        int getTop() {
+            if(top == -1)
+                cout << "Empty stack." << endl;
+            else
+                return arr[top];
+        }
 
-    int sizep() {
-        return top + 1;
-    }
+        //size
+        int getSize() {
+            return top+1;
+        }
 
-    bool empty() {
-        if(top == -1)
-            return true;
-        else    
-            return false;
-    }
-
+        //empty
+        bool empty() {
+            if(top == -1)
+                return true;
+            else 
+                return false;
+        }
 };
 
 int main() {
-    
-  //CREATION
-  Stack s(5);
 
-  //insertion
-  s.push(10);
-  s.push(20);
-  s.push(30);
-  s.push(40);
-  s.push(50);
-  s.push(60);
+    //creation
+    Stack s(5);
 
-  //check element on top
-  cout << "check element on top: " << s.topp() << endl;
+    //insertion
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
 
-  while(!s.empty()) {
-        cout << s.topp() << " ";
+    //size
+    cout << "Number of element in Stack: " << s.getSize() << endl;
+
+    //print
+    while (!s.empty()) {
+        cout << s.getTop() << " ";
         s.pop();
-  }cout << endl;
+    } cout << endl;
 
-  cout << "Size of stack " << s.sizep() << endl;
-
-  s.pop();
-
-  return 0;
+    //size
+    cout << "Number of element in Stack: " << s.getSize() << endl;
+    
+    return 0;
 }
