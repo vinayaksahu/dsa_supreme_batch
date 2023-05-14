1dcbabf8935e2d02506af76f6e77038ef44f745e
Vinayaksahu <vinayaksahu3@gmail.com>
2023-05-14 18:16:13 +0530

    min stack

---
+++ 11.Stack/Stack - 2/5.removeRedundantParenthisis.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+string removeRedundant(string s) {
+    
+}
+
+int main() {
+
+    string s = "(a+(b+c))";
+
+    string result = removeRedundant(s);
+
+    cout << result;
+
+    return 0;
+}
---
+++ 11.Stack/Stack - 3/1.minStack.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class MinStack {
+
+    public:
+
+    vector < pair <int, int> > st;
+
+    MinStack(int val) {
+    }
+
+    void push(int val) {
+        if (st.empty()) {
+            pair<int, int> p = make_pair(val, val);
+            st.push_back(p);
+        }
+        else {
+            pair<int, int> p;
+            p.first = val;
+            p.second = min(val, st.back().second);
+            st.push_back(p);
+        }
+    }
+
+    void pop() {
+        st.pop_back();
+    }
+
+    int top() {
+        return st.back().first;
+    }
+
+    int getMin() {
+        return st.back().second;
+    }
+};
+
+int main () {
+
+    return 0;
+}
