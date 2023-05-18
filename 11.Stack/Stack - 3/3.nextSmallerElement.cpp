a6efb6f587dece614d6cc71f2516a1e4e74405db
Vinayaksahu <vinayaksahu3@gmail.com>
2023-05-18 19:46:59 +0530

    next smaller element using stack

--- 11.Stack/Stack - 3/3.nextSmallerElement.cpp
+++ 11.Stack/Stack - 3/3.nextSmallerElement.cpp
@@ -3,26 +3,55 @@
 #include<vector>
 using namespace std;
 
-int main () {
+void print(vector<int> v) {
+    for (auto &&i : v) {
+        cout << i << " ";
+    } cout << endl;
+}
 
-    int arr[] = {4,3,2,1}; // 7,6,-1-1 // 6,6,-1,-1
-
-//brute force method:   
-    int n = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < n; i++) {
+void bruteForce(vector<int> arr) {
+    
+    for (int i = 0; i < arr.size(); i++) {
         int ans = -1;
-        for (int j = i+1; j < n; j++) {
+        for (int j = i+1; j < arr.size(); j++) {
             if (arr[i] > arr[j]) {
                 ans = arr[j];
                 break;
             }
-        }
+        } 
         cout << ans << " ";
     }
+    cout << endl;
+}
 
-//stack data structure method:
-
+void stackMethod(vector<int> input) {
     
+    vector<int> ans(input.size()); //create ans array size of input array
+    stack<int> s; //initialize stack
+    s.push(-1); //with -1
+    for (int i = input.size()-1; i >= 0; i--) { //traverse input array
+        int curr = input[i]; 
+        while (s.top() >= curr) { //remove top element of stack
+            s.pop(); //until you find curr element of input array is less then top element of array
+        }
+        ans[i] = s.top(); //if top element of stack found less then curr element store in ans array
+        s.push(curr); //then push curr element into stack
+    }
 
-    return 0;
+    print(ans); //print ans array
+}
+
+int main() {
+
+    vector<int> input;
+    input.push_back(4);
+    input.push_back(8);
+    input.push_back(9);
+    input.push_back(5);
+
+    print(input);
+
+    bruteForce(input);
+
+    stackMethod(input);
 }
