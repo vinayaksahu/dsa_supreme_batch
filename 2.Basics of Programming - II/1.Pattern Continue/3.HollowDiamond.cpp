474958d872e824a13da4251bf3dbc6375b6d219a
Vinayaksahu <vinayaksahu3@gmail.com>
2023-06-08 16:02:33 +0530

    organize folders, left hollow diamond

---
+++ 2.Basics of Programming - II/1.Pattern Continue/3.HollowDiamond.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n = 5;
+/* 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            cout << "*";
+        }
+        
+        for (int j = 0; j < i+1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < n-i; j++) {
+            cout << "*";
+        } cout << endl;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i+1; j++) {
+            cout << "*";
+        }
+        
+        for (int j = 0; j < n-i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < n-i-1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < i+1; j++) {
+            cout << "*";
+        } cout << endl;
+    }
+ */
+  
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2*n; j++) {
+            if (i == 0 || )
+                cout << "*";
+        } cout << endl;
+    }
+
+    return 0;
+}
+
+
+/*
+
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+*/
