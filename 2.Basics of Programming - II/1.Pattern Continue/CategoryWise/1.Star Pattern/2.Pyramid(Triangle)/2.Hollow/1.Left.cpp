6115c9c075014964c5dba5710ffce475270c0f46
Vinayaksahu <vinayaksahu3@gmail.com>
2023-06-09 13:45:14 +0530

    diamond pattern Lakshya bhaiya method

--- 2.Basics of Programming - II/1.Pattern Continue/CategoryWise/1.Star Pattern/2.Pyramid(Triangle)/2.Hollow/1.Left.cpp
+++ 2.Basics of Programming - II/1.Pattern Continue/CategoryWise/1.Star Pattern/2.Pyramid(Triangle)/2.Hollow/1.Left.cpp
@@ -7,7 +7,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i+1; j++) {
-            if (i == 0 || i == n-1 || j == 0 || j == i)
+            if (i == n-1 || j == 0 || j == i)
                 cout << "* ";
             else    
                 cout << "  ";
--- 2.Basics of Programming - II/1.Pattern Continue/CategoryWise/1.Star Pattern/3.Diamond/1.Solid/1.Natural.cpp
+++ 2.Basics of Programming - II/1.Pattern Continue/CategoryWise/1.Star Pattern/3.Diamond/1.Solid/1.Natural.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     
     int n = 5;
-
+/* 
         //Upper
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -24,7 +24,26 @@ int main() {
             cout << "* ";
         } cout << endl;    
     }
-    
+   */  
+   
+    for (int i = 0; i < 2*n-1; i++) {
+
+        int cond = 0;
+
+        if (i < n)
+            cond = i + 1;
+        else    
+            cond = n - (i % n) - 1;
+
+        for (int j = 0; j < n - cond; j++) {
+            cout << " ";   
+        }
+
+        for (int j = 0; j < cond; j++) {
+            cout << "* ";
+        } cout << endl;
+    }
+   
     return 0;
 }
 
