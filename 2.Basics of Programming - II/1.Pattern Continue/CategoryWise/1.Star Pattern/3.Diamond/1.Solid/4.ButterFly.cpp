2e66278d50ebcb340768c149d5be478f9c6752eb
Vinayaksahu <vinayaksahu3@gmail.com>
2023-06-10 23:55:11 +0530

    butterfly pattern final done

--- 2.Basics of Programming - II/1.Pattern Continue/CategoryWise/1.Star Pattern/3.Diamond/1.Solid/4.ButterFly.cpp
+++ 2.Basics of Programming - II/1.Pattern Continue/CategoryWise/1.Star Pattern/3.Diamond/1.Solid/4.ButterFly.cpp
@@ -7,17 +7,19 @@ int main() {
 
     for (int i = 0; i < 2*n; i++) {
 
-        int cond = (i < n) ? (i + 1) : (n - (i % n));
+        //int cond = (i < n) ? (i + 1) : (n - (i % n));
+        int cond = (i < n) ? (i + 1) : (2*n - i);
 
-        int space_count = (i < n) ? (2*(n - i - 1)) : (i - cond); 
+        //int space_count = (i < n) ? (2*(n - cond)) : (i - cond); 
+        int space_count = (i < n) ? (2*(n - i - 1)) : (2*(i - n)); 
 
         for (int j = 0; j < 2*n; j++) {
 
             if (j < cond)
                 cout << "*";
                 
-            else if (space_count-- > 0)
-                cout << " ";
+            else if (space_count-- > 0) //every iteration me space print hone k
+                cout << " ";            // liye ek ek space km hote jayegi
                 
             else
                 cout << "*";
