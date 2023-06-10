06231d04564c6c678e4c8938e4408c77e5231241
Vinayaksahu <vinayaksahu3@gmail.com>
2023-06-10 16:52:50 +0530

    .gitignore update

---
+++ 2.Basics of Programming - II/1.Pattern Continue/5.FancyPattern2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    return 0;
+}
+
+/*
+
+1
+2 3
+4 5 6
+7 8 9 10
+7 8 9 10
+4 5 6
+2 3
+1
+
+*/
