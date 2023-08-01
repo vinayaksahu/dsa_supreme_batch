2ca43740454b339b2b9c89513387049451952971
Vinayaksahu <vinayaksahu3@gmail.com>
2023-08-01 11:34:21 +0530

    linked list in c

---
+++ 10.Linked List/Linked List - 2/linkedList.c
@@ -0,0 +1,286 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertAtBeginning(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = head;
+    return newNode;
+}
+
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) {
+        return newNode;
+    } else {
+        struct Node* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+        return head;
+    }
+}
+
+struct Node* insertAfterNode(struct Node* prevNode, int data) {
+    if (prevNode == NULL) {
+        return NULL;
+    }
+    struct Node* newNode = createNode(data);
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+    return prevNode;
+}
+
+void traverse(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+struct Node* getNode(struct Node* head, int data) {
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->data == data) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+struct Node* deleteFromBeginning(struct Node* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    struct Node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
+struct Node* deleteFromEnd(struct Node* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if (head->next == NULL) {
+        free(head);
+        return NULL;
+    }
+    struct Node* current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+    free(current->next);
+    current->next = NULL;
+    return head;
+}
+
+struct Node* deleteNode(struct Node* head, int data) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if (head->data == data) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    struct Node* current = head;
+    while (current->next != NULL) {
+        if (current->next->data == data) {
+            struct Node* temp = current->next;
+            current->next = current->next->next;
+            free(temp);
+            return head;
+        }
+        current = current->next;
+    }
+    return head;
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insertAtBeginning(head, 4);
+    head = insertAtBeginning(head, 3);
+    head = insertAtBeginning(head, 2);
+    head = insertAtBeginning(head, 1);
+
+    traverse(head);
+
+    head = insertAtEnd(head, 5);
+
+    traverse(head);
+
+    struct Node* targetNode = getNode(head, 3);
+    if (targetNode != NULL) {
+        insertAfterNode(targetNode, 6);
+    }
+
+    traverse(head);
+
+    head = deleteFromBeginning(head);
+    head = deleteFromEnd(head);
+    head = deleteNode(head, 3);
+
+    traverse(head);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void insertAfterNode(struct Node* prevNode, int data) {
+    if (prevNode == NULL) {
+        return;
+    }
+    struct Node* newNode = createNode(data);
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+}
+
+void traverse(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+struct Node* getNode(struct Node* head, int data) {
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->data == data) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+void deleteFromBeginning(struct Node** head) {
+    if (*head == NULL) {
+        return;
+    }
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+void deleteFromEnd(struct Node** head) {
+    if (*head == NULL) {
+        return;
+    }
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+    struct Node* current = *head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+    free(current->next);
+    current->next = NULL;
+}
+
+void deleteNode(struct Node** head, int data) {
+    if (*head == NULL) {
+        return;
+    }
+    if ((*head)->data == data) {
+        struct Node* temp = *head;
+        *head = (*head)->next;
+        free(temp);
+        return;
+    }
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        if (current->next->data == data) {
+            struct Node* temp = current->next;
+            current->next = current->next->next;
+            free(temp);
+            return;
+        }
+        current = current->next;
+    }
+}
+
+/* int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 4);
+    insertAtBeginning(&head, 3);
+    insertAtBeginning(&head, 2);
+    insertAtBeginning(&head, 1);
+
+    traverse(head);
+
+    insertAtEnd(&head, 5);
+
+    traverse(head);
+
+    struct Node* targetNode = getNode(head, 3);
+    if (targetNode != NULL) {
+        insertAfterNode(targetNode, 6);
+    }
+
+    traverse(head);
+
+    deleteFromBeginning(&head);
+    deleteFromEnd(&head);
+    deleteNode(&head, 3);
+
+    traverse(head);
+
+    return 0;
+} */
