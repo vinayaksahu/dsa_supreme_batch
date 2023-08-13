be44cabf21216a3de6642415992c6b37aeece7de
Vinayaksahu <vinayaksahu3@gmail.com>
2023-08-13 22:38:03 +0530

    spiral print

--- 3.Arrays Time & Space Comlexity/3.Array-2D/HomeWork/SpiralPrint.cpp
+++ 3.Arrays Time & Space Comlexity/3.Array-2D/HomeWork/SpiralPrint.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-void printArray(vector<vector<int>> arr, int m, int n) {
+void print(vector<vector<int>> arr, int m, int n) {
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -11,14 +11,33 @@ void printArray(vector<vector<int>> arr, int m, int n) {
     } cout << endl;
 }
 
-void spiral(vector<vector<int>> mat, int m, int n) {
+void spiral(vector<vector<int>> mat, int i, int j, int m, int n) {
 
-    vector<vector<int>> spiral(m, vector<int>(n));
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            spiral[i][j] = mat[j][i];
-        }   
-    }
+    // If i or j lies outside the matrix
+    if (i >= m or j >= n)
+        return;
+ 
+    // Print First Row
+    for (int p = j; p < n; p++)
+        cout << mat[i][p] << " ";
+ 
+    // Print Last Column
+    for (int p = i + 1; p < m; p++)
+        cout << mat[p][n - 1] << " ";
+ 
+    // Print Last Row, if Last and
+    // First Row are not same
+    if ((m - 1) != i)
+        for (int p = n - 2; p >= j; p--)
+            cout << mat[m - 1][p] << " ";
+ 
+    // Print First Column,  if Last and
+    // First Column are not same
+    if ((n - 1) != j)
+        for (int p = m - 2; p > i; p--)
+            cout << mat[p][j] << " ";
+ 
+    print(mat, i + 1, j + 1, m - 1, n - 1);
 
     //print 
     printArray(spiral, m, n);
@@ -35,10 +54,10 @@ int main () {
     int col = mat[0].size();
 
     cout <<"Before Spiral: " << endl;
-    printArray(mat, row, col);
+    print(mat, row, col);
 
     cout <<"After Spiral: " << endl;
-    spiral(mat, row, col);
+    spiral(mat, 0, 0, row, col);
 
     return 0;
 }
