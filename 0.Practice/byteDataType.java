ecc1cb13e7ce2cb2dfba4b2cf6ab788048f2ab1c
Vinayaksahu <vinayaksahu3@gmail.com>
2024-01-19 11:46:43 +0530

    java byte code datatype typecasting

---
+++ 0.Practice/byteDataType.java
@@ -0,0 +1,9 @@
+class Simple{  
+    public static void main(String[] args){  
+        //Overflow  
+        int a=129;  
+        byte b=(byte)a;  
+        System.out.println(a);  
+        System.out.println(b);  
+    }
+}  
