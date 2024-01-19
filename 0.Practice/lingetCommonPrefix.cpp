102cf4c33f2897ff763fd738e38d1390788d23e9
Vinayaksahu <vinayaksahu3@gmail.com>
2024-01-19 13:01:21 +0530

    longest common prefix

---
+++ 0.Practice/lingetCommonPrefix.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        sort(strs.begin(), strs.end());
+        int n = strs.size();
+        string first = strs[0];
+        string last = strs[n-1];
+        for(int i = 0; i < min(first.size(), last.size()); i++) {
+            if(first[i] != last[i]) {
+                return ans;
+            }
+            ans += first[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    // Example usage
+    vector<string> strings = {"flower", "flow", "flight"};
+    string result = solution.longestCommonPrefix(strings);
+    
+    cout << "Longest Common Prefix: " << result << endl;
+
+    return 0;
+}
